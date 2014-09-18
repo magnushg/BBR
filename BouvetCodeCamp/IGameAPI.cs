ee4a6afbe12c2d061925819da1b182822e637ed5
magnushg <magnus.h.green@gmail.com>
2014-09-18 22:32:38 +0200

    Fjernet avhengighet til http fra IGameAPI og GameAPI

--- BouvetCodeCamp/IGameAPI.cs
+++ BouvetCodeCamp/IGameAPI.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
+using BouvetCodeCamp.Felles.Entiteter;
 using BouvetCodeCamp.InputModels;
 using BouvetCodeCamp.OutputModels;
 
@@ -8,14 +9,14 @@ namespace BouvetCodeCamp
 {
     public interface IGameApi
     {
-        Task<HttpResponseMessage> RegistrerPifPosition(GeoPosisjonModel model);
+        Task<PifPosisjon> RegistrerPifPosition(GeoPosisjonModel model);
 
         Task<PifPosisjonModel> HentSistePifPositionForLag(string lagId);
 
         Task<IEnumerable<PifPosisjonModel>> HentAllePifPosisjoner();
-        
-        HttpResponseMessage RegistrerKode(KodeModel model);
 
-        HttpResponseMessage SendMelding(MeldingModel model);
+        void RegistrerKode(KodeModel model);
+
+        void SendMelding(MeldingModel model);
     }
 }
