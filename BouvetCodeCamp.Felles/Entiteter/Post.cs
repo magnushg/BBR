6062ecccb71586182d50325c096025569a6e6fd6
Henrik W. Moe <henrik.moe@bouvet.no>
2014-09-21 15:10:08 +0200

    Litt boyscouting på tester

--- BouvetCodeCamp.Felles/Entiteter/Post.cs
+++ BouvetCodeCamp.Felles/Entiteter/Post.cs
@@ -1,15 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Policy;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BouvetCodeCamp.Felles.Entiteter
 {
     public class Post : BaseDocument
     {
-        public string Id { get; set; }
         public DateTime OpprettetDato { get; set; }
         public string Latitude { get; set; }
         public string Longitude { get; set; }
