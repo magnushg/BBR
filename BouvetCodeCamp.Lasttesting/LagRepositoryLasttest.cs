b932303f9bb7335abeaff6f8755ee10c227428b9
Henrik W. M. <henrik.moe@bouvet.no>
2014-09-24 10:07:22 +0200

    Lastteste oppretting/henting av mange lag

--- BouvetCodeCamp.Lasttesting/LagRepositoryLasttest.cs
+++ BouvetCodeCamp.Lasttesting/LagRepositoryLasttest.cs
@@ -2,53 +2,42 @@
 
 namespace BouvetCodeCamp.Lasttesting
 {
-    using System.Linq;
+    using System;
 
-    using BouvetCodeCamp.Domene.Entiteter;
-    using BouvetCodeCamp.Infrastruktur;
-    using BouvetCodeCamp.Infrastruktur.Repositories;
     using BouvetCodeCamp.Integrasjonstester;
     using BouvetCodeCamp.Integrasjonstester.DataAksess;
 
-    using FizzWare.NBuilder;
-
     using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-    using Should;
 
     [TestClass]
     public class LagRepositoryLasttest : BaseRepositoryIntegrasjonstest
     {
         [TestMethod]
         [TestCategory(Testkategorier.Last)]
-        public async Task Opprett_10000Lag_10000LagErOpprettet()
+        public void Lasttest_Hent_LagMedLagId_LagHarLagId()
         {
             // Arrange
-            var repository = OpprettRepository();
+            var lagRepositoryIntegrasjonstester = new LagRepositoryIntegrasjonstester();
             
-            var mangeLag = Builder<Lag>.CreateListOfSize(10000)
-                .All()
-                .Random(10000)
-                .Build().ToList();
-            
-            foreach (var lag in mangeLag)
+            const int AntallTesterSomSkalKjøres = 10;
+
+            try
             {
-                // Act
-                var documentId = await repository.Opprett(lag);
-                var opprettetLag = await repository.Hent(documentId);
+                var parallelleTasker = new Task[AntallTesterSomSkalKjøres];
 
-                var lagDocument = await repository.Hent(opprettetLag.DocumentId);
-                
-                // Assert
-                lagDocument.DocumentId.ShouldNotBeEmpty();
+                for (var i = 0; i < parallelleTasker.Length; i++)
+                {
+                    parallelleTasker[i] = new Task(() => lagRepositoryIntegrasjonstester.Hent_LagMedLagId_LagHarLagId());
+                }
 
-                break;
+                Task.WaitAll(parallelleTasker);
             }
-        }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
 
-        private LagRepository OpprettRepository()
-        {
-            return new LagRepository(new Konfigurasjon(), new DocumentDbContext(new Konfigurasjon()));
+            Assert.IsTrue(true);
         }
     }
 }
