bb82ae827508d0ab1704f16ce4503a5959031049
magnushg <magnus.h.green@gmail.com>
2014-09-30 19:41:11 +0200

    La til modifisert fil for kodeposter og legger postkoder på riktig plass i importen

--- BouvetCodeCamp.SpillOppretter/LagOppretter.cs
+++ BouvetCodeCamp.SpillOppretter/LagOppretter.cs
@@ -27,6 +27,9 @@ namespace BouvetCodeCamp.SpillOppretter
         public IEnumerable<Lag> OpprettLag(IEnumerable<Post> poster)
         {
             var lagPosterJson = File.ReadAllText("importData/lagPoster.json", Encoding.UTF8);
+            var posterMedKoderJson = File.ReadAllText("importData/koder.json", Encoding.UTF8);
+            
+            var posterMedKoder = JsonConvert.DeserializeObject<IEnumerable<PosterMedKoder>>(posterMedKoderJson);
 
             var lagListe = Enumerable.Range(1, _antallLag).Select(index => new Lag
             {
@@ -53,17 +56,17 @@ namespace BouvetCodeCamp.SpillOppretter
                 Meldinger = lag.Meldinger,
                 Poster = lagPoster
                     .FirstOrDefault(lagPost => lagPost.Lagnummer == lag.LagNummer)
-                    .Poster.Select(post => OpprettLagPost(poster.FirstOrDefault(p => p.Nummer == post))).ToList()
+                    .Poster.Select(post => OpprettLagPost(poster.FirstOrDefault(p => p.Nummer == post), posterMedKoder, lag.LagNummer)).ToList()
 
             }).ToList();
-            
+
             LagreLagliste(lagListeMedPoster);
 
             return lagListe;
 
         }
 
-        private LagPost OpprettLagPost(Post post)
+        private LagPost OpprettLagPost(Post post, IEnumerable<PosterMedKoder> posterMedKoder, int lagNummer)
         {
             return new LagPost
             {
@@ -75,7 +78,7 @@ namespace BouvetCodeCamp.SpillOppretter
                 Nummer = post.Nummer,
                 Posisjon = post.Posisjon,
                 PostTilstand = PostTilstand.Ukjent,
-                Kode = string.Empty
+                Kode = posterMedKoder.FirstOrDefault(p => p.Postnr == post.Nummer).Koder[lagNummer-1]
             };
         }
 
@@ -94,5 +97,13 @@ namespace BouvetCodeCamp.SpillOppretter
         public int Lagnummer { get; set; }
         public int[] Poster { get; set; }
     }
+
+
+    public class PosterMedKoder
+    {
+        public int Postnr { get; set; }
+        public string[] Koder { get; set; }
+    }
+
 
 }
