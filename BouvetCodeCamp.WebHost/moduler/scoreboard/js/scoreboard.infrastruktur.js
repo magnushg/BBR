85faee7d888db198493e491c3f81d40785eec045
Henrik W. M. <henrik.moe@bouvet.no>
2014-10-08 08:29:13 +0200

    Opprettet js-filer for å skille ut js-kode. Flyttet ut sendAutentisertRequest()

---
+++ BouvetCodeCamp.WebHost/moduler/scoreboard/js/scoreboard.infrastruktur.js
@@ -0,0 +1,24 @@
+﻿var infrastruktur = (function($) {
+    'use strict';
+
+    function sendAutentisertRequest(url, type, successCallback) {
+        $.ajax({
+            url: url,
+            type: type,
+            dataType: 'json',
+            success: successCallback,
+            beforeSend: setAuthorizationHeader
+        });
+    }
+
+    function setAuthorizationHeader(xhr) {
+        xhr.setRequestHeader('Authorization', 'Basic Ym91dmV0Om15c2VjcmV0');
+    }
+
+    return {
+        sendAutentisertRequest: function (url, type, successCallback) {
+            sendAutentisertRequest(url, type, successCallback);
+        }
+    }
+
+})($);
