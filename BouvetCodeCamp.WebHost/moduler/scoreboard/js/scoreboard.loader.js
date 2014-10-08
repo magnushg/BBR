dabe835fc2b28f4e687ee90cd00bd387319d48f7
Henrik W. M. <henrik.moe@bouvet.no>
2014-10-08 21:16:43 +0200

    Flyttet js-kode inn i loader

--- BouvetCodeCamp.WebHost/moduler/scoreboard/js/scoreboard.loader.js
+++ BouvetCodeCamp.WebHost/moduler/scoreboard/js/scoreboard.loader.js
@@ -1,1 +1,30 @@
-﻿
+﻿var loader = (function ($, infrastruktur) {
+    'use strict';
+
+    function lastInnLag(onSuccessCallback) {
+        infrastruktur.sendAutentisertRequest("/api/admin/lag/get", "GET", onSuccessCallback);
+    }
+
+    function lastInnPoster(onSuccessCallback) {
+        infrastruktur.sendAutentisertRequest("/api/admin/post/get", "GET", onSuccessCallback);
+    } 
+
+    function lastInnInfisertSone(onSuccessCallback) {
+        infrastruktur.sendAutentisertRequest("/api/admin/infisert/get", "GET", onSuccessCallback);
+    }
+
+    return {
+        lastInnLag: function (onSuccessCallback) {
+            lastInnLag(onSuccessCallback);
+        },
+
+        lastInnPoster: function (onSuccessCallback) {
+            lastInnPoster(onSuccessCallback);
+        },
+
+        lastInnInfisertSone: function (onSuccessCallback) {
+            lastInnInfisertSone(onSuccessCallback);
+        }
+    }
+
+}($, infrastruktur));
