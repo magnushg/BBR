873a4329f6da1017a3b6743e52d68387afab0ed3
Henrik W. M. <henrik.moe@bouvet.no>
2014-11-06 09:51:09 +0100

    Flyttet på tester til å gjenspeile prosjektstrukturen

---
+++ Bouvet.BouvetBattleRoyale.Unittester/Tjenester/GameApiTest.cs
@@ -0,0 +1,254 @@
+﻿namespace Bouvet.BouvetBattleRoyale.Unittests
+{
+    using System.Collections.Generic;
+
+    using Bouvet.BouvetBattleRoyale.Domene;
+    using Bouvet.BouvetBattleRoyale.Domene.Entiteter;
+    using Bouvet.BouvetBattleRoyale.Domene.InputModels;
+    using Bouvet.BouvetBattleRoyale.Infrastruktur.Interfaces;
+    using Bouvet.BouvetBattleRoyale.Tjenester;
+    using Bouvet.BouvetBattleRoyale.Tjenester.Interfaces;
+    using Bouvet.BouvetBattleRoyale.Tjenester.Interfaces.Services;
+    using Bouvet.BouvetBattleRoyale.Tjenester.Interfaces.SignalR.Hubs;
+
+    using Moq;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class GameApiTest
+    {
+        private IGameApi _gameApi;
+        private readonly Mock<IPostGameService> _postGameService = new Mock<IPostGameService>();
+        private readonly Mock<ILagGameService> _lagGameService = new Mock<ILagGameService>();
+        private readonly Mock<IService<Lag>> _lagService = new Mock<IService<Lag>>();
+        private readonly Mock<IService<GameState>> _gameStateService = new Mock<IService<GameState>>();
+        private readonly Mock<IKoordinatVerifier> _koordinatVerifier = new Mock<IKoordinatVerifier>();
+        private readonly Mock<IPoengService> _poengServiceMock = new Mock<IPoengService>();
+        private readonly Mock<IGameHub> _gameHubMock = new Mock<IGameHub>();
+        private readonly Mock<IArkivHandler> _arkivHandlerMock = new Mock<IArkivHandler>();
+
+        [SetUp]
+        public void Setup()
+        {
+            _gameApi = new GameApi(
+                _postGameService.Object,
+                _lagGameService.Object,
+                _lagService.Object,
+                _koordinatVerifier.Object,
+                _gameStateService.Object,
+                _poengServiceMock.Object,
+                _gameHubMock.Object,
+                _arkivHandlerMock.Object
+                );
+        }
+
+        [Test]
+        [ExpectedException(typeof(MeldingException))]
+        public async void SendMelding_Lengde_IkkeInt_Exception()
+        {
+            var melding = new MeldingInputModell
+            {
+                Type = MeldingType.Lengde,
+                Innhold = "juks"
+            };
+            await _gameApi.SendMelding(melding);
+        }
+
+        [Test]
+        [ExpectedException(typeof(MeldingException))]
+        public async void SendMelding_Fritekst_TekstOver256_Exception()
+        {
+            var melding = new MeldingInputModell
+            {
+                Type = MeldingType.Fritekst,
+                Innhold = "UTiBm8m3wOvmBLrwO26NRPHI2o7pwPUSuxPcjn1A6ybVZ88OBwsWO9Z0FAfLtowUjwJGJLIg4BNXS8GozxBqZjKgE3WUcfNhbBUxgJVEty4LJwpCvTjkSux1njsA6pG0TDmrE04v62kHmwE0zip2gP5XG0Ew43G3hg4KhjYWDtQ5bTmKB15qkXqU0gYTZUTR10ZGIzoeaYuvVDOoc1CHvwSApqkMUoNRycnv1QzH62pk7SPY8n5HxlfMnsF8eJSfI"
+            };
+            Assert.IsTrue(melding.Innhold.Length > 256);
+            await _gameApi.SendMelding(melding);
+        }
+
+        [Test]
+        public async void SendMelding_Fritekst_ErUnder256_Gyldig()
+        {
+            var melding = new MeldingInputModell
+            {
+                Type = MeldingType.Fritekst,
+                Innhold = "UTiBm8m3wOvmBLrwO26NRPHI2o7pwPUSuxPcjn1A6ybVZ88OBwsWO9Z0FAfLtowUjwJGJLIg4BNXS8GozxBqZjKgE3WUcfNhbBUxgJVEty4LJwpCvTjkSux1njsA6pG0TDmrE04v62kHmwE0zip2gP5XG0Ew43G3hg4KhjYWDtQ5bTmKB15qkXqU0gYTZUTR10ZGIzeaYuvVDOoc1CHvwSApqkMUoNRycnv1QzH62pk7SPY8n5HxlfMnsF8eJSfI"
+            };
+            _lagGameService.Setup(x => x.HentLagMedLagId(It.IsAny<string>())).Returns(new Lag
+            {
+                Meldinger = new List<Melding>()
+            });
+
+            await _gameApi.SendMelding(melding);
+
+            Assert.IsTrue(melding.Innhold.Length <= 256);
+            Assert.IsTrue(true);
+        }
+
+
+        [Test]
+        [ExpectedException(typeof(MeldingException))]
+        public async void SendMelding_Stopp_NotBool_Exception()
+        {
+            var melding = new MeldingInputModell
+            {
+                Type = MeldingType.Stopp,
+                Innhold = "juks"
+            };
+
+            await _gameApi.SendMelding(melding);
+        }
+
+        [Test]
+        public async void SendMelding_Stopp_Bool_Success()
+        {
+            var melding = new MeldingInputModell
+            {
+                Type = MeldingType.Stopp,
+                Innhold = "false"
+            };
+            _lagGameService.Setup(x => x.HentLagMedLagId(It.IsAny<string>())).Returns(new Lag
+            {
+                Meldinger = new List<Melding>()
+            });
+
+            await _gameApi.SendMelding(melding);
+            Assert.IsTrue(true);
+        }
+
+        [Test]
+        [ExpectedException(typeof(MeldingException))]
+        public async void SendMelding_Himmelretning_Invalid_Exception()
+        {
+            var melding = new MeldingInputModell
+            {
+                Type = MeldingType.Himmelretning,
+                Innhold = "ikke en himmelretning"
+            };
+
+            await _gameApi.SendMelding(melding);
+        }
+
+        [Test]
+        public async void SendMelding_Himmelretning_North_Success()
+        {
+            var melding = new MeldingInputModell
+            {
+                Type = MeldingType.Himmelretning,
+                Innhold = "North"
+            };
+            _lagGameService.Setup(x => x.HentLagMedLagId(It.IsAny<string>())).Returns(new Lag
+            {
+                Meldinger = new List<Melding>()
+            });
+
+            await _gameApi.SendMelding(melding);
+            Assert.IsTrue(true);
+        }
+
+        [Test]
+        public async void SendMelding_Himmelretning_East_Success()
+        {
+            var melding = new MeldingInputModell
+            {
+                Type = MeldingType.Himmelretning,
+                Innhold = "East"
+            };
+            _lagGameService.Setup(x => x.HentLagMedLagId(It.IsAny<string>())).Returns(new Lag
+            {
+                Meldinger = new List<Melding>()
+            });
+
+            await _gameApi.SendMelding(melding);
+            Assert.IsTrue(true);
+        }
+
+        [Test]
+        public async void SendMelding_Himmelretning_West_Success()
+        {
+            var melding = new MeldingInputModell
+            {
+                Type = MeldingType.Himmelretning,
+                Innhold = "West"
+            };
+            _lagGameService.Setup(x => x.HentLagMedLagId(It.IsAny<string>())).Returns(new Lag
+            {
+                Meldinger = new List<Melding>()
+            });
+
+            await _gameApi.SendMelding(melding);
+            Assert.IsTrue(true);
+        }
+
+        [Test]
+        public async void SendMelding_Himmelretning_South_Success()
+        {
+            var melding = new MeldingInputModell
+            {
+                Type = MeldingType.Himmelretning,
+                Innhold = "South"
+            };
+            _lagGameService.Setup(x => x.HentLagMedLagId(It.IsAny<string>())).Returns(new Lag
+            {
+                Meldinger = new List<Melding>()
+            });
+
+            await _gameApi.SendMelding(melding);
+            Assert.IsTrue(true);
+        }
+
+        [Test]
+        [ExpectedException(typeof(MeldingException))]
+        public async void SendMelding_Himmelretning_Integer_Exception()
+        {
+            var melding = new MeldingInputModell
+            {
+                Type = MeldingType.Himmelretning,
+                Innhold = "1"
+            };
+            _lagGameService.Setup(x => x.HentLagMedLagId(It.IsAny<string>())).Returns(new Lag
+            {
+                Meldinger = new List<Melding>()
+            });
+
+            await _gameApi.SendMelding(melding);
+            Assert.IsTrue(true);
+        }
+        [Test]
+        [ExpectedException]
+        public async void SendMelding_Himmelretning_Null_Exception()
+        {
+            var melding = new MeldingInputModell
+            {
+                Type = MeldingType.Himmelretning,
+                Innhold = null
+            };
+            _lagGameService.Setup(x => x.HentLagMedLagId(It.IsAny<string>())).Returns(new Lag
+            {
+                Meldinger = new List<Melding>()
+            });
+
+            await _gameApi.SendMelding(melding);
+            Assert.IsTrue(true);
+        }
+
+
+        [Test]
+        public void ErLagPifInnenInfeksjonssone_ErInnenInfeksjonssone_ReturnsTrue()
+        {
+            var gamestate = new GameState { InfisertPolygon = new InfisertPolygon()};
+            var pif = new PifPosisjon();
+
+            _lagGameService.Setup(x => x.HentSistePifPosisjon(It.IsAny<string>())).Returns(() => pif);
+            _gameStateService.Setup(x => x.Hent(string.Empty)).Returns(() => gamestate);
+            _koordinatVerifier.Setup(
+                x => x.KoordinatErInnenforPolygonet(pif.Posisjon, gamestate.InfisertPolygon.Koordinater)).Returns(true);
+
+            var result = _gameApi.ErInfisiert(pif.Posisjon, gamestate);
+
+            Assert.IsTrue(result);
+        }
+    }
+}
---
+++ Bouvet.BouvetBattleRoyale.Unittester/Tjenester/Services/PoengServiceTest.cs
@@ -0,0 +1,313 @@
+﻿namespace Bouvet.BouvetBattleRoyale.Unittests.Service
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Bouvet.BouvetBattleRoyale.Domene;
+    using Bouvet.BouvetBattleRoyale.Domene.Entiteter;
+    using Bouvet.BouvetBattleRoyale.Infrastruktur.Interfaces;
+    using Bouvet.BouvetBattleRoyale.Tjenester.Interfaces;
+    using Bouvet.BouvetBattleRoyale.Tjenester.Interfaces.Services;
+    using Bouvet.BouvetBattleRoyale.Tjenester.Interfaces.SignalR.Hubs;
+    using Bouvet.BouvetBattleRoyale.Tjenester.Services;
+
+    using Moq;
+
+    using NUnit.Framework;
+
+    using Should;
+
+    [TestFixture]
+    class PoengServiceTest
+    {
+        private IPoengService _poengService;
+        private readonly Mock<IKoordinatVerifier> _coordinatMock = new Mock<IKoordinatVerifier>();
+        private readonly Mock<IGameHub> _gameHubMock = new Mock<IGameHub>();
+        private readonly Mock<IArkivHandler> _arkivHandlerMock = new Mock<IArkivHandler>();
+        
+        [SetUp]
+        public void Startup()
+        {
+            _poengService = new PoengService(_gameHubMock.Object, _arkivHandlerMock.Object);
+
+            PoengTildeling.InfisertTickStraff = 1;
+            PoengTildeling.InfisertTidssfrist = 1;
+            PoengTildeling.KodeOppdaget = 1;
+            PoengTildeling.MeldingsStraff = 1;
+            PoengTildeling.PingTimeoutStraff = 1;
+            PoengTildeling.PingTimeout = 1;
+        }
+
+        [Test]
+        public void SettFritekstMeldingSendtStraff_ikkeFritekst_IngenStraff()
+        {
+            PoengTildeling.MeldingsStraff = -2;
+            const int startPoeng = 10;
+            var melding = new Melding
+            {
+                Type = MeldingType.Himmelretning
+            };
+
+            var lag = new Lag {Poeng = startPoeng};
+            lag = _poengService.SettFritekstMeldingSendtStraff(lag, melding);
+
+            Assert.AreEqual(startPoeng, lag.Poeng);
+        }
+
+        [Test]
+        public void SettFritekstMeldingSendtStraff_fritekst_FarStraff()
+        {
+            PoengTildeling.MeldingsStraff = -2;
+            const int startPoeng = 10;
+            var melding = new Melding
+            {
+                Type = MeldingType.Fritekst,
+                Tekst = "123 5"
+            };
+            var length = melding.Tekst.Length;
+
+            var lag = new Lag {Poeng = startPoeng};
+            lag = _poengService.SettFritekstMeldingSendtStraff(lag, melding);
+
+            var forventetResultat = 0;
+            Assert.AreEqual(0, lag.Poeng);
+        }
+
+        [Test]
+        public void SjekkOgSettPifPingStraff_Timeout1Sekund_1PingStraff()
+        {
+            PoengTildeling.PingTimeout = 1;
+            PoengTildeling.PingTimeoutStraff = -1;
+            const int startPoeng = 10;
+
+            var lag = new Lag
+            {
+                PifPosisjoner = new List<PifPosisjon>
+                {
+                    new PifPosisjon { Tid = new DateTime(2000, 1, 1, 1, 1, 1)},
+                    new PifPosisjon { Tid = new DateTime(2000, 1, 1, 1, 1, 3)}
+                },
+                Poeng = startPoeng
+            };
+            lag = _poengService.SjekkOgSettPifPingStraff(lag);
+
+            lag.Poeng.ShouldEqual(9);
+        }
+
+        [Test]
+        public void SjekkOgSettPifPingStraff_TimeoutMed5Sekund_1PingStraff()
+        {
+            PoengTildeling.PingTimeout = 1;
+            PoengTildeling.PingTimeoutStraff = -10;
+
+            const int startPoeng = 10;
+            var lag = new Lag
+            {
+                PifPosisjoner = new List<PifPosisjon>
+                {
+                    new PifPosisjon { Tid = new DateTime(2000, 1, 1, 1, 1, 1)},
+                    new PifPosisjon { Tid = new DateTime(2000, 1, 1, 1, 1, 2)},
+                    new PifPosisjon { Tid = new DateTime(2000, 1, 1, 1, 1, 2 + 5)}
+                },
+                Poeng = startPoeng
+            };
+            lag = _poengService.SjekkOgSettPifPingStraff(lag);
+
+            lag.Poeng.ShouldEqual(0);
+        }
+
+        [Test]
+        public void SjekkOgSettPifPingStraff_2TimeOuts_2PingStraff()
+        {
+            PoengTildeling.PingTimeout = 1;
+            PoengTildeling.PingTimeoutStraff = -1;
+
+            const int startPoeng = 10;
+            var lag = new Lag
+            {
+                PifPosisjoner = new List<PifPosisjon>
+                {
+                    new PifPosisjon { Tid = new DateTime(2000, 1, 1, 1, 1, 8)},
+                    new PifPosisjon { Tid = new DateTime(2000, 1, 1, 1, 1, 2)},
+                    new PifPosisjon { Tid = new DateTime(2000, 1, 1, 1, 1, 1)},
+                },
+                Poeng = startPoeng
+            };
+
+            lag = _poengService.SjekkOgSettPifPingStraff(lag);
+
+            lag.Poeng.ShouldEqual(9);
+
+            lag.PifPosisjoner.Add(new PifPosisjon { Tid = new DateTime(2000, 1, 1, 1, 1, 10)});
+            lag = _poengService.SjekkOgSettPifPingStraff(lag);
+
+            lag.Poeng.ShouldEqual(8);
+        }
+
+
+        [Test]
+        public void SjekkOgSettPifPingStraff_NoTimeout_IngenStraff()
+        {
+            PoengTildeling.PingTimeout = 10;
+
+            var startPoeng = 10;
+            var lag = new Lag
+            {
+                PifPosisjoner = new List<PifPosisjon>
+                {
+                    new PifPosisjon { Tid = new DateTime(2000, 1, 1, 1, 1, 1)},
+                    new PifPosisjon { Tid = new DateTime(2000, 1, 1, 1, 1, 11)}
+                },
+                Poeng = 10
+            };
+            lag = _poengService.SjekkOgSettPifPingStraff(lag);
+
+            lag.Poeng.ShouldEqual(startPoeng);
+        }
+
+        [Test]
+        public void SjekkOgSettPifPingStraff_NoTimeout_IngenStraff2()
+        {
+            PoengTildeling.PingTimeout = 10;
+
+            var startPoeng = 10;
+            var lag = new Lag
+            {
+                PifPosisjoner = new List<PifPosisjon>
+                {
+                    new PifPosisjon { Tid = new DateTime(2000, 1, 1, 1, 1, 1)},
+                    new PifPosisjon { Tid = new DateTime(2000, 1, 1, 1, 1, 2)}
+                },
+                Poeng = 10
+            };
+            lag = _poengService.SjekkOgSettPifPingStraff(lag);
+
+            lag.Poeng.ShouldEqual(startPoeng);
+        }
+
+        [Test]
+        [Ignore]
+        public void SjekkOgSettInfisertSoneStraff_TidIkkeUtgatt_IngenStraff1()
+        {
+            PoengTildeling.InfisertTickStraff = 10;
+            PoengTildeling.InfisertTidssfrist = 5;
+
+            var startPoeng = 10;
+            var lag = new Lag
+            {
+                PifPosisjoner = new List<PifPosisjon>
+                {
+                    new PifPosisjon { Tid = new DateTime(2000, 1, 1, 1, 1, 1), Infisert = true },
+                    new PifPosisjon { Tid = new DateTime(2000, 1, 1, 1, 1, 2), Infisert = true },
+                    new PifPosisjon { Tid = new DateTime(2000, 1, 1, 1, 1, 3), Infisert = true },
+                    new PifPosisjon { Tid = new DateTime(2000, 1, 1, 1, 1, 4), Infisert = true },
+                    new PifPosisjon { Tid = new DateTime(2000, 1, 1, 1, 1, 5), Infisert = true },
+                    new PifPosisjon { Tid = new DateTime(2000, 1, 1, 1, 1, 6), Infisert = true }
+                },
+                Poeng = 10
+            };
+
+            lag = _poengService.SjekkOgSettInfisertSoneStraff(lag);
+
+            lag.Poeng.ShouldEqual(startPoeng);
+        }
+
+        [Test]
+        [Ignore]
+        public void SjekkOgSettInfisertSoneStraff_TidIkkeUtgatt_IngenStraff2()
+        {
+            PoengTildeling.InfisertTidssfrist = 5;
+            PoengTildeling.InfisertTickStraff = -1;
+
+            var startPoeng = 10;
+            var lag = new Lag
+            {
+                PifPosisjoner = new List<PifPosisjon>
+                {
+                    new PifPosisjon { Tid = new DateTime(2000, 1, 1, 1, 1, 2), Infisert = true },
+                    new PifPosisjon { Tid = new DateTime(2000, 1, 1, 1, 1, 1), Infisert = false },
+                    new PifPosisjon { Tid = new DateTime(2000, 1, 1, 1, 1, 3), Infisert = true },
+                    new PifPosisjon { Tid = new DateTime(2000, 1, 1, 1, 1, 5), Infisert = true },
+                    new PifPosisjon { Tid = new DateTime(2000, 1, 1, 1, 1, 4), Infisert = true },
+                    new PifPosisjon { Tid = new DateTime(2000, 1, 1, 1, 1, 6), Infisert = true }
+                },
+                Poeng = 10
+            };
+
+            lag = _poengService.SjekkOgSettInfisertSoneStraff(lag);
+
+            lag.Poeng.ShouldEqual(startPoeng);
+        }
+
+        [Test]
+        public void SjekkOgSettInfisertSoneStraff_InfisertI3Sekund_3TickStraff()
+        {
+            PoengTildeling.InfisertTickStraff = -1;
+            var tid = 3;
+            var startPoeng = 10;
+            var lag = new Lag
+            {
+                PifPosisjoner = new List<PifPosisjon>
+                {
+                    new PifPosisjon { Tid = new DateTime(2000, 1, 1, 1, 1, 1), Infisert = true },
+                    new PifPosisjon { Tid = new DateTime(2000, 1, 1, 1, 1, 2), Infisert = false },
+                    new PifPosisjon { Tid = new DateTime(2000, 1, 1, 1, 1, 3), Infisert = true },
+                    new PifPosisjon { Tid = new DateTime(2000, 1, 1, 1, 1, 3 + tid), Infisert = true },
+                },
+                Poeng = startPoeng
+            };
+
+            lag = _poengService.SjekkOgSettInfisertSoneStraff(lag);
+
+            Assert.AreEqual(7, lag.Poeng);
+        }
+
+        [Test]
+        [Ignore]
+        public void SjekkOgSettInfisertSoneStraff_TidUtgattMed1Sekund_1TickStraff()
+        {
+            PoengTildeling.InfisertTickStraff = -10;
+            PoengTildeling.InfisertTidssfrist = 5;
+
+            var startPoeng = 10;
+            var lag = new Lag
+            {
+                PifPosisjoner = new List<PifPosisjon>
+                {
+                    new PifPosisjon { Tid = new DateTime(2000, 1, 1, 1, 1, 1), Infisert = true },
+                    new PifPosisjon { Tid = new DateTime(2000, 1, 1, 1, 1, 1 + PoengTildeling.InfisertTidssfrist + 1), Infisert = true },
+                },
+                Poeng = startPoeng
+            };
+
+            lag = _poengService.SjekkOgSettInfisertSoneStraff(lag);
+
+            lag.Poeng.ShouldEqual(startPoeng + (int)PoengTildeling.InfisertTickStraff);
+        }
+
+        [Test]
+        [Ignore]
+        public void SjekkOgSettInfisertSoneStraff_TidUtgattMed3Sekund_3TickStraff()
+        {
+            PoengTildeling.InfisertTickStraff = 10;
+            PoengTildeling.InfisertTidssfrist = 5;
+
+            var startPoeng = 10;
+            var lag = new Lag
+            {
+                PifPosisjoner = new List<PifPosisjon>
+                {
+                    new PifPosisjon { Tid = new DateTime(2000, 1, 1, 1, 1, 6), Infisert = true },
+                    new PifPosisjon { Tid = new DateTime(2000, 1, 1, 1, 1, 5), Infisert = false },
+                    new PifPosisjon { Tid = new DateTime(2000, 1, 1, 1, 1, 6 + PoengTildeling.InfisertTidssfrist + 3), Infisert = true },
+                },
+                Poeng = 10
+            };
+
+            lag = _poengService.SjekkOgSettInfisertSoneStraff(lag);
+
+            lag.Poeng.ShouldEqual(startPoeng - (int)PoengTildeling.InfisertTickStraff * 3);
+        }
+
+    }
+}
